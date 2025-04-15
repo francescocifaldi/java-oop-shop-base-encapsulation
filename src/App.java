14c51be6646b5e9bc1981c84b50c1adbe114c5bc
francescocifaldi <francesco.cifaldi@gmail.com>
2025-04-15 14:49:31 +0200

    ADD getters, setters and methods

--- src/App.java
+++ src/App.java
@@ -2,6 +2,10 @@ import org.lessons.java.shop.Prodotto;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Prodotto p1 = new Prodotto("Caffè", "Lavazza", 2.5, 22);
+        Prodotto p2 = new Prodotto();
+
+        System.out.println("Prodotto 1: " + p1.getFullName() + ", Prezzo finito: " + p1.getPrezzoFinale());
+        System.out.println("Prodotto 2: " + p2.getFullName() + ", Prezzo finito: " + p2.getPrezzoFinale());
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -25,10 +25,64 @@ public class Prodotto {
         Random random = new Random();
         this.codice = random.nextInt(9999);
         this.nome = "Prodotto senza nome";
-        this.marca = "Prodotto senzaz marca";
+        this.marca = "Prodotto senza marca";
         this.prezzoBase = 0;
         this.iva = 22;
     }
 
     // getter e setter
+    public int getCodice() {
+        return codice;
+    }
+
+    public void setNome(String nome) {
+        nome = nome.substring(0, 1).toUpperCase() + nome.substring(1).toLowerCase();
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setMarca(String marca) {
+        marca = marca.substring(0, 1).toUpperCase() + marca.substring(1).toLowerCase();
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setPrezzoBase(double prezzoBase) {
+        if (prezzoBase < 0) {
+            prezzoBase = 0;
+        }
+        this.prezzoBase = prezzoBase;
+    }
+
+    public double getPrezzoBase() {
+        return prezzoBase;
+    }
+
+    public void setIva(int iva) {
+        if (iva < 0) {
+            iva = 0;
+        } else if (iva > 100) {
+            iva = 100;
+        }
+        this.iva = iva;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+
+    // metodi
+    public String getFullName() {
+        return this.codice + "-" + this.nome;
+    }
+
+    public double getPrezzoFinale() {
+        return this.prezzoBase + (this.prezzoBase * this.iva / 100);
+    }
 }
