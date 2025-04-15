457cd149fad49e36049090e43ff0910748d7fb2b
francescocifaldi <francesco.cifaldi@gmail.com>
2025-04-15 14:37:09 +0200

    ADD attributes and constructor to Prodotto class

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,9 +1,34 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
+
+    // attributi
     private int codice;
     private String nome;
     private String marca;
     private double prezzoBase;
     private int iva;
+
+    // costruttori
+    public Prodotto(String nome, String marca, double prezzoBase, int iva) {
+        Random random = new Random();
+        this.codice = random.nextInt(9999);
+        this.nome = nome;
+        this.marca = marca;
+        this.prezzoBase = prezzoBase;
+        this.iva = iva;
+    }
+
+    public Prodotto() {
+        Random random = new Random();
+        this.codice = random.nextInt(9999);
+        this.nome = "Prodotto senza nome";
+        this.marca = "Prodotto senzaz marca";
+        this.prezzoBase = 0;
+        this.iva = 22;
+    }
+
+    // getter e setter
 }
